2867cde8903ac595edf2c0126e218a6b3d0a1e7a
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-29 15:26:29 +0900

    update all

--- netflix-clone/src/main.js
+++ netflix-clone/src/main.js
@@ -2,21 +2,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // router 설정 가져오기
-import store from './store';
-import '@fortawesome/fontawesome-free/css/all.css';
-
-// FontAwesome 설정
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faAngleLeft, faAngleRight, faHeart, faShareNodes } from '@fortawesome/free-solid-svg-icons';
-
-// 필요한 아이콘 추가
-library.add(faAngleLeft, faAngleRight, faHeart, faShareNodes);
 
 const app = createApp(App);
 
 // Vue 앱에 플러그인 등록
 app.use(router);
-app.use(store);
 
 // FontAwesome 컴포넌트 등록
 
